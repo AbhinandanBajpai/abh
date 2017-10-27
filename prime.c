91459df23c02dd4c5f95661fe000d047d0516a6d
AbhinandanBajpai <33058249+AbhinandanBajpai@users.noreply.github.com>
2017-10-27 14:42:47 +0530

    Update prime.c

--- prime.c
+++ prime.c
@@ -3,7 +3,11 @@ void main()
 {
 int n,c=0;
 scanf("%d",&n);
-for(int i=2;i<=n/2;i++)
+  if(n==2)
+    printf("prime");
+  else
+  {
+  for(int i=2;i<=n/2;i++)
 {
 if(n%i==0)
 {
@@ -15,4 +19,5 @@ if(c==0)
 printf("prime");
 else
 printf("not a prime");
+  }
 }
