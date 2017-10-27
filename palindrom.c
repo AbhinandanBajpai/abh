c69f7895ae77aa93371ac2e275abbd980773fad5
AbhinandanBajpai <33058249+AbhinandanBajpai@users.noreply.github.com>
2017-10-27 14:30:17 +0530

    Create palindrom.c

---
+++ palindrom.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int n,r=0,t;
+scanf("%d",&n);
+t==n;
+while(n!=0)
+{
+r=r*10;
+r=r+n%10;
+n=n/10;
+}
+if(t==n)
+printf("palindrome");
+else
+printf("not a palindrome");
+}
